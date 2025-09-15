c62e1c12466cc685c217e7c83b0f54ba103d13da
Colby Sax <cjsax@csuchico.edu>
2025-09-14 21:03:07 -0700

    dice functional with visuals, ability to save/reroll

--- Yahtzee.py
+++ Yahtzee.py
@@ -4,50 +4,92 @@ import random
 import sys
 
 
-# pygame setup
+def getBorderColor(selected):
+    color = None
+    if selected:
+        color = "deepskyblue"
+    else:
+        color = "Black"
+
+    return color        
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 80)
+scoreFont = pygame.font.SysFont('Calibri', 25)
+
+
+dice = []
+selectedDie = [False] * 5
+diceXCords = [80, 321, 562, 803, 1044]
+diceYCord = 550
+
+
+buttonArea = pygame.Rect(340, 430, 600, 100)
+for i in range(0, 5):
+    dice.append(random.randint(1,6))
+
+
+screen.fill("lightgray")
+
 while running:
-    buttonCheck = False
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+#        elif event.type == pygame.KEYDOWN:
+#            if event.key == pygame.K_SPACE:
+        elif event.type  == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                print(event.pos)
+                if buttonArea.collidepoint(event.pos):
+                    for i in range(0, 5):
+                        if not selectedDie[i]:
+                            dice[i] = random.randint(1, 6)
+                for i in range(0, 5):
+                    if pygame.Rect(diceXCords[i], diceYCord, 156, 156).collidepoint(event.pos):
+                        if not selectedDie[i]:
+                            selectedDie[i] = True
+                        else:
+                            selectedDie[i] = False 
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
 
-    # RENDER YOUR GAME HERE
-    pygame.font.init() # you have to call this at the start, 
-                   # if you want to use this module.
-    my_font = pygame.font.SysFont('Comic Sans MS', 100)
+    for i in range(0, 5):
+        text_surface = my_font.render(str(dice[i]), False, (0, 0, 0))
+        screen.blit(text_surface, (diceXCords[i], diceYCord))
+        color = getBorderColor(selectedDie[i])
+        pygame.draw.rect(screen, color, pygame.Rect(diceXCords[i]-3, diceYCord-3, 162, 162)) 
+        diceImg = pygame.image.load("assets/dice" +str(dice[i])+ ".png")
+        screen.blit(diceImg, (diceXCords[i], diceYCord))
+        
 
 
-    dice1 = random.randint(1, 6)
-    text_surface = my_font.render(str(dice1), False, (0, 0, 0))
-    screen.blit(text_surface, (144,0)) #renders text to screen
-
+    pygame.draw.rect(screen, 'Black', pygame.Rect(440-4, 10-4, 408, 408)) 
+    tableImg = pygame.image.load("assets/table.png")
+    screen.blit(tableImg, (440, 10, 400, 400))
     
+    pygame.draw.rect(screen, "Black", pygame.Rect(340-3, 430-3, 606, 106))
+    pygame.draw.rect(screen, "gray79", buttonArea)
 
 
-    # flip() the display to put your work on screen
+    testScore = scoreFont.render("1", True, (0, 0, 0))
+    screen.blit(testScore, (563, 146))
+
+    testScore = scoreFont.render("1", True, (0, 0, 0))
+    screen.blit(testScore, (603, 146))
+
+
+    buttonText = my_font.render("Click to Reroll", True, (0, 0, 0))
+    screen.blit(buttonText, (450, 435)) 
+
+
+
     pygame.display.flip()
-
-    event = pygame.event.wait()
-
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()    
-
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            continue
-    else:
-        continue
 
     clock.tick(60)  # limits FPS to 60
 
