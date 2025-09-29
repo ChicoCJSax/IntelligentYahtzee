80fafd1ff9302811644a7135c18fc1e5e37ba4c6
Colby Sax <cjsax@csuchico.edu>
2025-09-29 00:55:40 -0700

    added scores/ability to save scores, limited dice rerolls

---
+++ Player.py
@@ -0,0 +1,89 @@
+from collections import Counter
+class Player:
+    
+    def isALargeStraight(dice):
+        noDupeDice = []
+        for i in dice:
+            if i not in noDupeDice:
+                noDupeDice.append(i)
+        noDupeDice.sort()
+        for i in range(min(noDupeDice), max(noDupeDice)):
+            if i not in noDupeDice:
+                return False
+            
+        return True
+    
+    def isASmallStraight(dice):
+        numbersets = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]
+        noDupeDice = set(dice)
+        for i in numbersets:
+            subsetCheck = i.issubset(noDupeDice)    
+            if subsetCheck:       
+                return True
+            
+        return False
+                          
+    def __init__(self):
+        self.scores = {
+            "1s": [0, False],
+            "2s": [0, False],
+            "3s": [0, False],
+            "4s": [0, False],
+            "5s": [0, False],
+            "6s": [0, False],
+            "ThreeOAK": [0, False],
+            "FourOAK": [0, False],
+            "fullhse": [0, False],
+            "sstr8": [0, False],
+            "lstr8": [0, False],
+            "yahtzee": [0, False],
+            "chance": [0, False]
+            }
+  
+    def setScore(self, type, scoreVal):
+        if self.scores[type][1] == False:
+            self.scores[type][0] = scoreVal
+  
+    def updateScores(self, currentDice):
+        dice = Counter(currentDice)     
+        for i in range(1, 7):
+            self.setScore(f"{i}s", dice[i] * i)
+
+        if max(dice.values()) >= 4:
+            self.setScore("FourOAK", sum(currentDice))
+
+        if max(dice.values()) >= 3:
+            self.setScore("ThreeOAK", sum(currentDice))
+            
+        if 3 in dice.values():
+            if 2 in dice.values():
+                self.setScore("fullhse", 25)
+
+        
+        if max(dice) - min(dice) == 4:
+            check = Player.isALargeStraight(dice)
+            if check == True:
+                self.setScore("lstr8", 40)
+
+
+        if Player.isASmallStraight(dice) == True:
+            self.setScore("sstr8", 30)
+
+        if 5 in dice.values():
+            self.setScore("yahtzee", 50)
+
+        self.setScore("chance", sum(currentDice))
+                        
+    def resetValues(self):
+        for key, value in self.scores.items():
+            if value[1] == False:
+                value[0] = 0
+                
+
+    def lockScore(self, scoreType):
+        self.scores[f"{scoreType}"][1] = True
+        
+        
+
+
+            
--- Yahtzee.py
+++ Yahtzee.py
@@ -2,6 +2,17 @@ import pygame
 import pygame.locals
 import random
 import sys
+import Player
+from collections import Counter
+
+def rerollDice(dice, selectedDice, freshDice = False):
+    if freshDice == True:
+        for i in range (0, 5):
+            selectedDice[i] = False
+            
+    for i in range(0, 5):
+        if not selectedDice[i]:
+            dice[i] = random.randint(1, 6)
 
 
 def getBorderColor(selected):
@@ -12,6 +23,33 @@ def getBorderColor(selected):
         color = "Black"
 
     return color        
+
+def getTextColor(scoreFlag):
+    color = None
+    if scoreFlag:
+        color = "Black"
+    else:
+        color = "deepskyblue"
+
+    return color        
+
+
+player1ScoreCords = {
+            "1s": [589, 96],
+            "2s": [589, 156],
+            "3s": [582, 208],
+            "4s": [577, 260],
+            "5s": [578, 306],
+            "6s": [580, 340],
+            "ThreeOAK": [682, 95],
+            "FourOAK": [682, 139],
+            "fullhse": [675, 183],
+            "sstr8": [675, 232],
+            "lstr8": [666, 278],
+            "yahtzee": [665, 314],
+            "chance": [675, 359]
+}
+
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -34,30 +72,31 @@ for i in range(0, 5):
     dice.append(random.randint(1,6))
 
 
-screen.fill("lightgray")
-
+player1 = Player.Player()
+rerollNum = 2
 while running:
-    
-    
+    screen.fill("lightgray")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-#        elif event.type == pygame.KEYDOWN:
-#            if event.key == pygame.K_SPACE:
         elif event.type  == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                print(event.pos)
-                if buttonArea.collidepoint(event.pos):
-                    for i in range(0, 5):
-                        if not selectedDie[i]:
-                            dice[i] = random.randint(1, 6)
+                if buttonArea.collidepoint(event.pos) and rerollNum > 0:
+                    rerollDice(dice, selectedDie)
+                    rerollNum -= 1
                 for i in range(0, 5):
                     if pygame.Rect(diceXCords[i], diceYCord, 156, 156).collidepoint(event.pos):
                         if not selectedDie[i]:
                             selectedDie[i] = True
                         else:
                             selectedDie[i] = False 
-
+                for key, value in player1ScoreCords.items():
+                    scoreArea = pygame.Rect(value[0]-5, value[1]-5, 35, 35)
+                    if scoreArea.collidepoint(event.pos):
+                        if player1.scores[key][1] == False:
+                            player1.lockScore(key)
+                            rerollDice(dice, selectedDie, freshDice=True)
+                            rerollNum = 2
 
     for i in range(0, 5):
         text_surface = my_font.render(str(dice[i]), False, (0, 0, 0))
@@ -69,6 +108,9 @@ while running:
         
 
 
+    rerollDisplay = scoreFont.render("You have " +str(rerollNum)+ " rerolls left", True, (0, 0, 0))
+    screen.blit(rerollDisplay, (37, 475))
+    
     pygame.draw.rect(screen, 'Black', pygame.Rect(440-4, 10-4, 408, 408)) 
     tableImg = pygame.image.load("assets/table.png")
     screen.blit(tableImg, (440, 10, 400, 400))
@@ -76,12 +118,15 @@ while running:
     pygame.draw.rect(screen, "Black", pygame.Rect(340-3, 430-3, 606, 106))
     pygame.draw.rect(screen, "gray79", buttonArea)
 
+    player1.resetValues()
+    player1.updateScores(dice)
 
-    testScore = scoreFont.render("1", True, (0, 0, 0))
-    screen.blit(testScore, (563, 146))
+    for key, value in player1ScoreCords.items():
+        textColor = getTextColor(player1.scores[key][1])
+        scoreText = scoreFont.render(str(player1.scores[key][0]), True, textColor)
+        screen.blit(scoreText, (value[0], value[1]))
 
-    testScore = scoreFont.render("1", True, (0, 0, 0))
-    screen.blit(testScore, (603, 146))
+
 
 
     buttonText = my_font.render("Click to Reroll", True, (0, 0, 0))
