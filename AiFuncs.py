6dc10954dac25feac0aa22f7d7da119427dbf38e
Colby Sax <cjsax@csuchico.edu>
2025-11-17 11:13:37 -0800

    added basic firebase implemenation, basic new screens, level 1 AI WIP

--- AiFuncs.py
+++ AiFuncs.py
@@ -1,8 +1,20 @@
 import random
 import Player
+from collections import Counter
+
+highPriority = ['yahtzee', 'lstr8',  'fullhse', 'FourOAK']
+midPriority = ['sstr8',"ThreeOAK", "6s", "5s", "4s"]
+lowPriority = ['chance',  '3s', '2s', '1s']
+
+
+smallStr8Hands = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]
+
+
+
+
 def getLevelZeroChoice(rerolls, botObj):
     decision = random.randint(0, 1)
-    if decision == 0 and rerolls > 0:
+    if rerolls > 0 and botObj.scores['sstr8'][0]>0:
         return "reroll"
     else:
         nonLockedScores = []
@@ -12,4 +24,48 @@ def getLevelZeroChoice(rerolls, botObj):
         
         return random.choice(nonLockedScores)
 
-            
+
+
+def getLevelOneChoice(botObj, rerolls,  currentDice, selectedDice):
+    
+    for i in (0, len(selectedDice)):
+        selectedDice[i] = False
+        
+    for i in range(0, len(highPriority)):
+        if(botObj.scores[highPriority[i]][0] > 0 and botObj.scores[highPriority[i]][1] == False):
+            print(highPriority[i] + " possible, returning")
+            return highPriority[i]
+
+    diceCounts = Counter(currentDice)
+    
+    for smallset in smallStr8Hands:
+        if(rerolls > 0 and smallset.issubset(set(currentDice))):
+            print("small straight found! finding dice to keep")
+            markedDice = []
+            for i in range(0, 5):
+                
+                #needs to check if currentdice was already marked:
+                #[1, 6, 3, 4, 2]
+                #[True, False, True, True, True]
+                #small straight found! finding dice to keep
+                #[1, 3, 3, 4, 2]
+                #[True, True, True, True, True]
+                
+                if(currentDice[i] not in markedDice and currentDice[i] in smallset):
+                    selectedDice[i] = True
+                    markedDice.append(currentDice[i])
+                
+                elif(currentDice[i] in markedDice and currentDice[i] in smallset and selectedDice[i] ==True):
+                    selectedDice[i] = False
+            print(currentDice)
+            print(selectedDice)
+            return "reroll"
+                
+    
+    #TEST FOR SELECTING DICE TO KEEP
+    #for i in range(0, len(selectedDice)):
+    #    selectedDice[i] = not selectedDice[i]            
+    
+
+    
+    return
--- Yahtzee.py
+++ Yahtzee.py
@@ -6,6 +6,9 @@ import Player
 from collections import Counter
 import time
 import AiFuncs
+import firebase_options
+import os
+import json
 
 def rerollDice(dice, selectedDice, freshDice = False):
     if freshDice == True:
@@ -27,9 +30,9 @@ def drawScores(playerObj, key, value):
     textColor = getScoreColor(playerObj.scores[key][1])
     scoreText = scoreFont.render(str(playerObj.scores[key][0]), True, textColor)
     if(len(str(playerObj.scores[key][0])) == 1):
-        screen.blit(scoreText, (value[0]+4, value[1]))
+        game.blit(scoreText, (value[0]+4, value[1]))
     else:
-        screen.blit(scoreText, (value[0]-2, value[1]))
+        game.blit(scoreText, (value[0]-2, value[1]))
     
 def getScoreColor(locked):
     color = None
@@ -83,13 +86,63 @@ botScoreCords = {
 
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+game = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 80)
 scoreFont = pygame.font.SysFont('Calibri', 25)
+
+
+initialScreen = pygame.display.set_mode((1280, 720))
+loggedIn = False
+
+emailRect = pygame.Rect(440, 300, 400, 50)
+passwordRect = pygame.Rect(440, 380, 400, 50)
+loginButton = pygame.Rect(540, 470, 200, 60)
+
+
+
+if os.path.exists(".userinfo"):
+    with open(".userinfo", 'r') as file:
+        user = json.load(file)
+    loggedIn = True
+
+
+while not loggedIn:
+    initialScreen.fill("lightgray")
+    
+
+    helloText = scoreFont.render("Welcome to Intelligent Yahtzee! Please sign in below.", True, "black")
+    initialScreen.blit(helloText, (50, 150))
+
+    email = "test@email.com"
+    password = "password123"
+
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if loginButton.collidepoint(event.pos):
+                    try:
+                        user = firebase_options.login(email, password)
+                        with open(".userinfo", 'w') as file:
+                            json.dump(user, file)
+                        loggedIn = True
+                    except:
+                        print("Error logging in!")
+                    
+    pygame.draw.rect(initialScreen, "Black", pygame.Rect(540-3, 470-3, 206, 66))
+    pygame.draw.rect(initialScreen, "gray79", loginButton)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+
 
 
 dice = []
@@ -108,13 +161,18 @@ bot = Player.Player()
 playerScore = 0
 botScore = 0
 playerBonus = 0
+playerBonusTrigged = False
+
 botBonus = 0
+botBonusTriggered = False
 
 rerollNum = 2
 playerTurn = True
 
+playerInteracted = False
+
 while running:
-    screen.fill("lightgray")
+    game.fill("lightgray")
     flag = False #check to see if any score values are unlocked/equal False
     for val in player.scores.values():
         if val[1] == False:
@@ -129,11 +187,21 @@ while running:
         print("The bot's final score is "+str(botScore))
         time.sleep(5)
         running = False
-        
+    
+    
+    if(playerBonus>= 63 and playerBonusTrigged == False):
+        playerScore+=35
+        playerBonusTrigged = True
+    
     if playerTurn == False:
         botChoice = AiFuncs.getLevelZeroChoice(rerollNum, bot)
+    
+    
+    
+        AiFuncs.getLevelOneChoice(bot, rerollNum, dice, selectedDie)
         time.sleep(1.5)
         if botChoice == "reroll":
+            time.sleep(7)
             rerollDice(dice, selectedDie)
             rerollNum -= 1
         else:
@@ -150,6 +218,7 @@ while running:
         elif event.type  == pygame.MOUSEBUTTONDOWN and playerTurn == True:
             if event.button == 1:
                 if buttonArea.collidepoint(event.pos) and rerollNum > 0:
+                    playerInteracted = True
                     rerollDice(dice, selectedDie)
                     rerollNum -= 1
                 for i in range(0, 5):
@@ -161,30 +230,34 @@ while running:
                 for key, value in playerScoreCords.items():
                     scoreArea = pygame.Rect(value[0]-5, value[1]-5, 30, 30)
                     if scoreArea.collidepoint(event.pos):
+                        playerInteracted = True
                         if player.scores[key][1] == False:
                             playerScore += player.scores[key][0] 
                             player.lockScore(key)
+                            
                             rerollDice(dice, selectedDie, freshDice=True)
                             rerollNum = 2
                             playerTurn = False
 
+
     for i in range(0, 5):
         text_surface = my_font.render(str(dice[i]), False, (0, 0, 0))
-        screen.blit(text_surface, (diceXCords[i], diceYCord))
+        game.blit(text_surface, (diceXCords[i], diceYCord))
         color = getBorderColor(selectedDie[i])
-        pygame.draw.rect(screen, color, pygame.Rect(diceXCords[i]-3, diceYCord-3, 162, 162)) 
+        pygame.draw.rect(game, color, pygame.Rect(diceXCords[i]-3, diceYCord-3, 162, 162)) 
         diceImg = pygame.image.load("assets/dice" +str(dice[i])+ ".png")
-        screen.blit(diceImg, (diceXCords[i], diceYCord))
+        game.blit(diceImg, (diceXCords[i], diceYCord))
 
     rerollDisplay = scoreFont.render("You have " +str(rerollNum)+ " rerolls left", True, (0, 0, 0))
-    screen.blit(rerollDisplay, (37, 475))
+    game.blit(rerollDisplay, (37, 475))
     
-    pygame.draw.rect(screen, 'Black', pygame.Rect(440-4, 10-4, 408, 408)) 
+    pygame.draw.rect(game, 'Black', pygame.Rect(440-4, 10-4, 408, 408)) 
     tableImg = pygame.image.load("assets/table.png")
-    screen.blit(tableImg, (440, 10, 400, 400))
+    game.blit(tableImg, (440, 10, 400, 400))
     
-    pygame.draw.rect(screen, "Black", pygame.Rect(340-3, 430-3, 606, 106))
-    pygame.draw.rect(screen, "gray79", buttonArea)
+    pygame.draw.rect(game, "Black", pygame.Rect(340-3, 430-3, 606, 106))
+    pygame.draw.rect(game, "gray79", buttonArea)
+
 
     player.resetValues()
     player.updateScores(dice)
@@ -194,36 +267,81 @@ while running:
 
 
     for key, value in playerScoreCords.items():
-        pygame.draw.rect(screen, "cornflowerblue", pygame.Rect(value[0]-5, value[1]-5, 30, 30))
+        pygame.draw.rect(game, "cornflowerblue", pygame.Rect(value[0]-5, value[1]-5, 30, 30))
         drawPlayerScores(player, key, value, turnCheck=playerTurn)
 
 
     for key, value in botScoreCords.items():
-        pygame.draw.rect(screen, "red", pygame.Rect(value[0]-5, value[1]-5, 30, 30))
+        pygame.draw.rect(game, "red", pygame.Rect(value[0]-5, value[1]-5, 30, 30))
         drawPlayerScores(bot, key, value, turnCheck= not playerTurn)
 
 
     playerDisplay = scoreFont.render("Your score is "+str(playerScore), True, (0, 0, 0))
-    screen.blit(playerDisplay, (37, 200))
+    game.blit(playerDisplay, (37, 200))
     botDisplay = scoreFont.render("The bot's score is "+str(botScore), True, (0, 0, 0))
-    screen.blit(botDisplay, (37, 300))
+    game.blit(botDisplay, (37, 300))
 
-    #bonusText = scoreFont.render(str(playerScore) + "/63", True, (0, 0, 0))
-    #screen.blit(bonusText, (572, 380))
+    playerBonus = player.calculateBonus()
+    bonusText = scoreFont.render(str(playerBonus) + "/63", True, (0, 0, 0))
+    game.blit(bonusText, (572, 380))
 
 
     if playerTurn == False:
         turnDisplay = scoreFont.render("It is currently the bot's turn.", True, (0, 0, 0))
     else:
         turnDisplay = scoreFont.render("It is currently your turn.", True, (0, 0, 0))
-    screen.blit(turnDisplay, (900, 300))
+    game.blit(turnDisplay, (900, 300))
 
 
     buttonText = my_font.render("Click to Reroll", True, (0, 0, 0))
-    screen.blit(buttonText, (450, 435)) 
+    game.blit(buttonText, (450, 435)) 
 
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
+
+endScreenShow = True
+endScreen = pygame.display.set_mode((1280, 720))
+
+
+currentUserMax = firebase_options.getMaxScore(user)
+try:
+    if currentUserMax<playerScore:
+        firebase_options.saveMaxScore(user, playerScore)
+        currentUserMax = playerScore
+except:
+    print("Error returned, setting with score of 0")
+    firebase_options.saveMaxScore(user, 0)
+    currentUserMax = playerScore
+
+if playerInteracted == True:
+    
+    while endScreenShow:
+        endScreen.fill("lightgray")
+        
+        finalText = scoreFont.render("You won!", True, "Black")
+        endScreen.blit(finalText, (600, 150))
+        
+        scoreText = scoreFont.render("Your score was " +str(playerScore), True, "Black")
+        endScreen.blit(scoreText, (600, 500))
+        
+
+
+        maxScoreText = scoreFont.render("Your highest ever score was " +str(currentUserMax) , True, "Black")
+        endScreen.blit(maxScoreText, (600, 550))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                endScreenShow = False
+
+        #Can retool into "Play again?" Button            
+        #pygame.draw.rect(game, "Black", pygame.Rect(340-3, 430-3, 606, 106))
+        #pygame.draw.rect(game, "gray79", loginButton)
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+
 
 pygame.quit()
